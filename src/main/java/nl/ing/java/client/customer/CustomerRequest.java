2968bfea7e09d65dc46e9afdb9667ddbef1be365
Deniz Silahcilar <dsilahcilar@gmail.com>
2018-03-22 00:31:43 +0100

    fix

--- src/main/java/nl/ing/java/client/customer/CustomerRequest.java
+++ src/main/java/nl/ing/java/client/customer/CustomerRequest.java
@@ -5,15 +5,10 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotNull;
-
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @ToString
 public class CustomerRequest {
-    @NotNull
-    @Email
     private String email;
 }
--- src/main/java/nl/ing/java/client/customer/CustomerResponse.java
+++ src/main/java/nl/ing/java/client/customer/CustomerResponse.java
@@ -3,11 +3,9 @@ package nl.ing.java.client.customer;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-import javax.validation.constraints.NotNull;
 import java.util.Collection;
 
 @AllArgsConstructor
-@NotNull
 @Getter
 public class CustomerResponse {
     private Collection<Container> containers;
