41966926c2a37e781f6941592e441a21aa663fc2
Deniz Silahcilar <dsilahcilar@gmail.com>
2018-03-21 20:55:29 +0100

    initial

---
+++ src/main/java/nl/ing/java/client/login/LoginClient.java
@@ -0,0 +1,51 @@
+package nl.ing.java.client.login;
+
+import nl.ing.java.guild.common.login.LoginRequest;
+import nl.ing.java.guild.common.login.LoginResponse;
+import retrofit2.Call;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+
+import java.io.IOException;
+
+public class LoginClient {
+    private final Retrofit retrofit;
+    private static LoginClient instance;
+
+    public LoginClient(Retrofit retrofit) {
+        this.retrofit = retrofit;
+    }
+
+    public LoginResponse login(String userName, String password) throws IOException {
+
+        LoginService loginService = retrofit.create(LoginService.class);
+        LoginRequest loginRequest = new LoginRequest(userName,password);
+        Call<LoginResponse> login = loginService.login(loginRequest);
+
+        Response<LoginResponse> pps = login.execute();
+        if (pps.isSuccessful()) {
+            System.out.println("pps = " + pps.body());
+            return pps.body();
+        }
+        throw new RuntimeException();
+    }
+
+    public static LoginClient build(String url) {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(url)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        instance = new LoginClient(retrofit);
+        return instance;
+    }
+
+    public interface LoginService {
+        @POST("/login")
+        Call<LoginResponse> login(@Body LoginRequest body);
+    }
+
+}
