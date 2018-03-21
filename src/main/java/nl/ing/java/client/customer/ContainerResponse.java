2756b352c4d44a096577b2e2a1bd91ed64ca9a0f
Marek Kapowicki <marek.kapowicki@ing.com>
2018-03-21 23:34:30 +0100

    add sample response

--- src/main/java/nl/ing/java/client/customer/ContainerResponse.java
+++ src/main/java/nl/ing/java/client/customer/ContainerResponse.java
@@ -1,4 +1,20 @@
 package nl.ing.java.client.customer;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@ToString
+@Builder
 public class ContainerResponse {
+    private String containerId;
+    private String name;
+    private String size;
+    private String location;
+    private String info;
 }
