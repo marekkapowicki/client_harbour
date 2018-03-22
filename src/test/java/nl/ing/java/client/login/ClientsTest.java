c52895dad3db5c15f0c71728ae8f79e1bd275eac
Marek Kapowicki <marek.kapowicki@ing.com>
2018-03-22 03:19:22 +0100

    add port data importer

--- src/test/java/nl/ing/java/client/login/ClientsTest.java
+++ src/test/java/nl/ing/java/client/login/ClientsTest.java
@@ -1,6 +1,7 @@
 package nl.ing.java.client.login;
 
 import nl.ing.java.client.customer.PortClient;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -9,12 +10,14 @@ import java.io.IOException;
 public class ClientsTest {
 
     @Test
+    @Ignore
     public void login() throws IOException {
         LoginClient loginClient = LoginClient.build("http://10.1.3.229:9090/api/");
         loginClient.login("ashish.jain@ing.com","somePassword");
     }
 
     @Test
+    @Ignore
     public void port() throws IOException {
         PortClient portClient = PortClient.build("http://localhost:8888/port/api/");
         portClient.getAll();
